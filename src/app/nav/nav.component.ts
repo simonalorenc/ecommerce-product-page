dcd892b269e9467ce7af8b7c32ae92ed64d77bc8
Simona Lorenc <lorenc.simona@gmail.com>
2023-10-04 08:31:11 +0200

    add addToCart

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss']
 })
-export class NavComponent {
+export class NavComponent implements OnChanges{
+  @Input() counter!: number
 
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes)
+  }
 }
